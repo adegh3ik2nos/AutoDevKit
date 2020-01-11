5757cad078536df64eacd622a651c124b6240977
adegh3ik2nos <adegh3ik2nos@gmail.com>
2020-01-11 16:20:13 +0900

    feat: CUi実行の自動化を追加
    入力ありきのCUIインストーラなどの実行を自動化

---
+++ cui_exec/src/main.rs
@@ -0,0 +1,105 @@
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate clap;
+
+mod settings;
+
+use std::{env, str, fs};
+use clap::{App, Arg, ArgMatches, SubCommand};
+use std::path::Path;
+
+
+enum ReturnCode {
+	Success, 
+	Unknown, 
+}
+
+fn main() {
+    
+	env::set_var("RUST_LOG", "debug");
+	env_logger::init();
+
+	//コマンドライン引数をパース
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+		.about(crate_description!())
+        .subcommand(SubCommand::with_name("new")
+            .about("create template settings.")
+            .arg(Arg::with_name("settings")
+                .help("settings name.")
+				.required(true)
+            )
+            .arg(Arg::with_name("dir")
+				.help("working directory.")
+				.short("d")
+				.long("dir")
+				.takes_value(true)
+            )
+		)
+        .subcommand(SubCommand::with_name("exec")
+            .about("auto execute cui tools.")
+            .arg(Arg::with_name("settings")
+                .help("settings name.")
+				.required(true)
+            )
+            .arg(Arg::with_name("dir")
+				.help("working directory.")
+				.short("d")
+				.long("dir")
+				.takes_value(true)
+            )
+		);
+	let matches = app.get_matches();
+
+	//exeのディレクトリ
+	//let current_dir: String = format!("{}", env::current_exe().unwrap().parent().unwrap().display());
+
+    let mut is_success = false;
+    
+	if let Some(sub_matches) = matches.subcommand_matches("new") {
+		let mut dir = ".";
+		if let Some(value) = sub_matches.value_of("dir") {
+			dir = value.trim_end_matches('/').trim_end_matches('\\');
+		}
+		if let Some(name) = sub_matches.value_of("settings") {
+            new(dir, name).unwrap_or_else(|e|{
+                error!("[new] command failed: {}", e);
+            });
+            is_success = true;
+		}
+    }
+	if let Some(sub_matches) = matches.subcommand_matches("exec") {
+		let mut dir = ".";
+		if let Some(value) = sub_matches.value_of("dir") {
+			dir = value.trim_end_matches('/').trim_end_matches('\\');
+		}
+		if let Some(name) = sub_matches.value_of("settings") {
+            exec(dir, name).unwrap_or_else(|e|{
+                error!("[exec] command failed: {}", e);
+            });
+            is_success = true;
+		}
+    }
+
+    if is_success {
+        info!("successful");
+        std::process::exit(ReturnCode::Success as i32);
+    } else {
+        error!("unknown failed");
+        std::process::exit(ReturnCode::Unknown as i32);
+    }
+}
+
+
+fn new(dir: &str, name: &str) -> Result<(), failure::Error> {
+    let settings = settings::Settings::default();
+    fs::create_dir_all(dir)?;
+    settings::write_settings(&format!("{}/{}.toml", dir, name), &settings)
+}
+
+fn exec(dir: &str, name: &str) -> Result<(), failure::Error> {
+    let settings = settings::Settings::new(&format!("{}/{}.toml", dir, name))?;
+    settings::exec_command(&settings)
+}
---
+++ cui_exec/src/settings.rs
@@ -0,0 +1,94 @@
+use std::fs::File;
+use std::io::Write;
+use std::{fs};
+use serde_derive::{Serialize, Deserialize};
+use failure;
+use std::process::{Command, Stdio};
+use std::path::Path;
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Settings {
+    work_dir: String,
+    terminal: String, 
+
+    command: String, 
+    inputs: Vec<String>, 
+    result: i32, 
+
+    chain_settings: String, 
+
+    failed_command: String, 
+}
+
+impl Settings {
+    pub fn new(path: &str) -> Result<Self, failure::Error> {
+        let text = fs::read_to_string(&path)?;
+        let settings = toml::from_str(&text)?;
+        Ok(settings)
+    }
+
+    pub fn default() -> Self {
+        Settings{
+            work_dir: ".".to_string(), 
+            terminal: "".to_string(), 
+
+            command: "".to_string(), 
+            inputs: Vec::new(), 
+            result: 0, 
+
+            chain_settings: "".to_string(), 
+            
+            failed_command: "".to_string(), 
+        }
+    }
+}
+
+pub fn write_settings(path: &str, settings: &Settings) -> Result<(), failure::Error> {
+    let mut file = File::create(path)?;
+    let text = toml::to_string(settings)?;
+    write!(&mut file, "{}", text)?;
+    Ok(())
+}
+
+pub fn exec_command(settings: &Settings) -> Result<(), failure::Error> {
+    match _exec_command(&settings.terminal, &settings.work_dir, &settings.command, &settings.inputs, settings.result, true) {
+        Ok(()) => {
+            if Path::new(&settings.chain_settings).exists() {
+                let chain_settings = Settings::new(&settings.chain_settings)?;
+                return exec_command(&chain_settings);
+            }
+        }, 
+        Err(e) => {
+            return _exec_command(&settings.terminal, &settings.work_dir, &settings.failed_command, &Vec::new(), 0, false);
+        }
+    }
+
+    Ok(())
+}
+
+fn _exec_command(terminal: &str, work_dir: &str, command: &str, inputs: &Vec<String>, result: i32, is_check_result: bool) -> Result<(), failure::Error> {
+    let mut process = Command::new(terminal)
+    .current_dir(work_dir)
+    .arg(command)
+    .stdin(Stdio::piped())
+    .spawn()?;
+
+    //入力が指定されていれば入力
+    if inputs.len() > 0 {
+        match process.stdin.as_mut() {
+            Some(console) => {
+                console.write_all((inputs.join("\n") + "\n").as_bytes())?;
+            }
+            None => {
+                return Err(failure::err_msg("mut console get failed."));
+            },
+        }
+    }
+
+    if result != process.wait()?.code().unwrap_or(result + 1) {
+		return Err(failure::err_msg("execute failed."));
+    }
+
+    Ok(())
+}
